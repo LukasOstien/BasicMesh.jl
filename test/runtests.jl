f2f8d40faf35893840f84d06981899827ccd3de3
LukasOstien <lukas.ostien@gmail.com>
2026-01-19 18:31:22 -0800

    Adding More Tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,6 +3,21 @@ using Test
 
 @testset "BasicMesh.jl" begin
     a,b = squaremesh([0,1,0,1],0.25);
-    @test size(a) == (25, 2);
-    @test size(b) == (32, 3);
+    c,d = circlemesh(0,0,1,0.25);
+    e,f,g = uniformrefine(a,b);
+    h,i,j = uniformrefineCircle(c,d,1);
+    @testset "generating shapes" begin
+        @test size(a) == (25, 2);
+        @test size(b) == (32, 3);
+        @test size(c) == (61, 2);
+        @test size(d) == (96, 3);
+    end
+    @testset "refining shapes" begin
+        @test size(e) == (81, 2)
+        @test size(f) == (128, 3)
+        @test size(g) == (56, 3)
+        @test size(h) == (217, 2)
+        @test size(i) == (384, 3)
+        @test size(j) == (156, 3)
+    end
 end
